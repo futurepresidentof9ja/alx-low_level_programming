2765b773c0e990c2dd08c8fb8323cd8d3d8f5f20
futurepresidentof9ja <paulolubowale47@gmail.com>
2022-06-05 17:58:34 -0700

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point 
+ *Return: Always 0 (Success)
+ */
+
+int main()
+{
+	char str[] = ("Programming is like building a multilingual puzzle\n")
+	
+	puts(str); /*writing the string to stdout*/
+}
+	    return (0);

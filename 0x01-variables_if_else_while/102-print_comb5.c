3eb38c08922405ae40f59f1f2345e674a314ce3b
futurepresidentof9ja <paulolubowale47@gmail.com>
2022-06-12 09:16:22 -0700

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,24 +7,25 @@
 int main(void)
 {
 int i, k;
-for (i = 0; i <= 99; i++)
+for (i = 0; i < 100; i++)
 {
-for (k = i + 1; k <= 100; k++) 
+for (k = 0; k < 100; k++)
 {
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
+if (i < k)
+{
+putchar((i / 10) + '48');
+putchar((i % 10) + '48');
 putchar(' ');
-putchar((k / 10) + '0');
-putchar((k % 10) + '0');
-
-if (!(i == 98 && k == 99))
+putchar((k / 10) + '48');
+putchar((k % 10) + '48');
+if (!(i == 98 && k == 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
+}
 putchar('\n');
-
 return (0);
 }

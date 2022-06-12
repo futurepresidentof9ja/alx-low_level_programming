de9b87ac0d8b1f192f6608a6a6b9823705070c49
futurepresidentof9ja <paulolubowale47@gmail.com>
2022-06-12 05:41:30 -0700

    task undone

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *  *   * main - Prints 3 combination of numbers
+ *   *     *
+ *    *       * Return: Always (Success)
+ *     **/
+int main(void)
+
+{
+	int i, k;
+
+for (i = 0; i <= 100; c++)
+{
+for (k = 0; k <= 100; k++)
+{
+if (i < k);
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

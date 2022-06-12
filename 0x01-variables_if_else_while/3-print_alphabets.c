caa907e4f01d9cb59236b0cba99d87adcd622127
futurepresidentof9ja <paulolubowale47@gmail.com>
2022-06-12 02:56:01 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,14 +13,12 @@ int main(void)
 {
 	char low;
 
-for (low = 'a'; < = 'z'; low++)
+for (low = 'a'; low <= 'z'; low++)
 
 putchar(low);
-
-for (low = 'A'; low < = 'Z'; low++)
+for (low = 'A'; low <= 'Z'; low++)
 
 putchar(low);
 putchar('\n');
-
 return (0);
 }

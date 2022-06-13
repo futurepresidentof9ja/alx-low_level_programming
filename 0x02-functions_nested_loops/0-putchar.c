028e5afd41d7f8f0f82e5042dcc731cc1da4fec5
futurepresidentof9ja <paulolubowale47@gmail.com>
2022-06-13 09:02:06 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**main - Entry point
+*a program that prints _putchar
+*Return: Always 0 (Success)
+*/
+int main(void)
+
+{
+
+char text[9] = "_putchar";
+int i = 0;
+
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+return (0);
+}

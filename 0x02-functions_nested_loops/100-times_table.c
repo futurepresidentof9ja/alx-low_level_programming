5887788fce2b08e818133bfef260bf7d7eb647a9
futurepresidentof9ja <paulolubowale47@gmail.com>
2022-06-14 10:55:40 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,7 @@
 */
 void print_times_table(int n)
 {
+
 int a = 0, rep, b;
 
 if (n < 0 || n > 15)
@@ -32,7 +33,7 @@ _putchar('0' + rep / 10);
 _putchar('0' + rep % 10);
 }
 else
-}
+{
 _putchar('0' + rep / 100);
 _putchar('0' + (rep - 100) / 10);
 _putchar('0' + rep % 10);

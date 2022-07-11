0e5ae63006f2e0864e64d015d3aefd5da401d154
Olubowale Paul <70038029+futurepresidentof9ja@users.noreply.github.com>
2022-07-11 19:32:15 +0100

    Add files via upload

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _islower - check if character is lower case
+*
+* @c: character to be checked
+* Return: 1 if lowercase 0 otherwhise
+*/
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
